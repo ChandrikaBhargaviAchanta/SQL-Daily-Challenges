3c118f964a0f7bea20631dcb8a21c3f69e6bccf0
Chandrika Bhargavi Achanta <chandrikaachanta@gmail.com>
2025-04-20 20:04:48 -0400

    Problem 10

--- 10. Average Time of Process per Machine.sql
+++ 10. Average Time of Process per Machine.sql
@@ -1,1 +1,14 @@
-
+SELECT 
+    start_logs.machine_id,
+    ROUND(AVG(end_logs.timestamp - start_logs.timestamp), 3) AS processing_time
+FROM 
+    Activity start_logs
+JOIN 
+    Activity end_logs
+ON 
+    start_logs.machine_id = end_logs.machine_id AND
+    start_logs.process_id = end_logs.process_id AND
+    start_logs.activity_type = 'start' AND
+    end_logs.activity_type = 'end'
+GROUP BY 
+    start_logs.machine_id;
